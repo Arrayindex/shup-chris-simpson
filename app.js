23d31844b70876377caad0d15fb329c53f57b7f4
Andrew Ally <andrew@plynk.me>
2017-10-23 15:59:03 +0100

    Added Phrases

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var _ = require('lodash');
 var app = express();
 
 var raw = require('./data/characters.json');
+var phrases = require('./data/phrases.json')
 
 app.set('json spaces', 4);
 
@@ -26,7 +27,13 @@ app.get('/simpsons/:id', function(req, res) {
   res.send(_.find(raw.data, {
     '_id': req.params.id
   }));
-})
+});
+
+app.get('/simpsons/:id/phrases', function(req, res) {
+  res.send(_.find(phrases.data, {
+    'character': req.params.id
+  }));
+});
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = app

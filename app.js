7e739a5a622b3bbeb94f363c77d250f71cf7081e
Andrew Ally <andrew@plynk.me>
2017-10-23 14:59:40 +0100

    test123

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var _ = require('lodash');
 var app = express();
 
 var raw = require('./data/characters.json');
+
+app.set('json spaces', 4);
 
 app.get('/', function(req, res) {
   res.send({
@@ -18,6 +21,12 @@ app.post('/', function(req, res) {
 app.get('/simpsons/', function(req, res) {
   res.send(raw.data);
 });
+
+app.get('/simpsons/:id', function(req, res) {
+  res.send(_.find(raw.data, {
+    '_id': id
+  }));
+})
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = app
